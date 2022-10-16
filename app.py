3facfde7eca7a6fc1f726ca510fa02e66c96a4f5
Grant Alphenaar <gdalphenaar@gmail.com>
2022-10-15 22:25:17 -0400

    update link routing

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ Bootstrap(app)
 pages = FlatPages(app)
 freezer = Freezer(app)
 repo = 'CIS641-HW2-Alphenaar' # change to github repo name
-root = ''
 
 
 # configuration stuff
@@ -37,39 +36,35 @@ app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 app.config['FREEZER_DESTINATION'] = './docs'
 
 
+# home page
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', root=root)
+    return render_template('index.html', title='Home')
 
 
+# about me page
 @app.route('/about-me/')
 def about():
-    return render_template('about-me.html', title='About Me', root=root)
-
-
-@app.route('/interests/')
-def interests():
-    return render_template('interests.html', title='Home', root=root)
+    return render_template('about-me.html', title='About Me')
 
 
 # page to hold ''blog posts''
 @app.route('/projects/')
-def ramblings():
+def projects():
     latest = sorted(pages, reverse=True, key=lambda p: p.meta['date'])
     published = (p for p in latest if 'published' in p.meta)
-    return render_template('projects.html', pages=published, root=root)
+    return render_template('projects.html', pages=published)
 
 
 # markdown ''blog'' pages
 @app.route('/projects/<path:path>/')
 def page(path):
     template = pages.get_or_404(path)
-    return render_template('page.html/', title=template['short-title'], page=template, root=root)
+    return render_template('page.html/', title=template['short-title'], page=template)
 
 
 # freezes app and serves static files - to run normally, use Flask run
 if __name__ == '__main__':
-    root = '/'+repo # set up gh-pages path
-    app.config['FREEZER_BASE_URL'] = 'http://localhost{}'.format(root)
+    app.config['FREEZER_BASE_URL'] = 'http://localhost{}'.format('/'+repo)
     freezer.freeze()  # freeze
     freezer.run(debug=True)  # serve
